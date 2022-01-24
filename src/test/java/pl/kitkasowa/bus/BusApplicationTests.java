6a7ab1a490598582ac7caa2673d799abcb13a839
Arkadiusz <areksowa1@gmail.com>
2022-01-24 21:00:49 +0100

    Initial commit

---
+++ src/test/java/pl/kitkasowa/bus/BusApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kitkasowa.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
